b1160d6a3105551403a21a4ac3f68e076da2b1ab
Lee Bradley <lee.bradley@mademedia.co.uk>
2018-10-16 16:08:08 +0100

    Add `Colour` FieldType
    Also update to be compatible with SS 4.1 and only require admin
    instead of CMS

---
+++ code/FieldType/Colour.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace MadeHQ\FieldType;
+
+use SilverStripe\ORM\FieldType\DBVarchar;
+use MadeHQ\Forms\ColourPicker;
+
+class Colour extends DBVarchar
+{
+
+    private static $casting = array(
+        'Hash' => 'Text',
+        'Red' => 'Text',
+        'Blue' => 'Text',
+        'Green' => 'Text',
+    );
+
+    public function __construct($name = null, $options = array())
+    {
+        parent::__construct($name, 7, $options);
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see DBField::scaffoldFormField()
+     */
+    public function scaffoldFormField($title = null, $params = null)
+    {
+        return new ColourPicker($this->name, $title);
+    }
+
+    public function getHash()
+    {
+        return substr($this->value, 1);
+    }
+
+    public function getRGB()
+    {
+        $hash = $this->getHash();
+        return array(
+            'R' => hexdec(substr($hash, 0, 2)),
+            'G' => hexdec(substr($hash, 2, 2)),
+            'B' => hexdec(substr($hash, 4, 2)),
+        );
+    }
+
+    public function getRed()
+    {
+        return $this->getRGB()['R'];
+    }
+
+    public function getBlue()
+    {
+        return $this->getRGB()['B'];
+    }
+
+    public function getGreen()
+    {
+        return $this->getRGB()['G'];
+    }
+}
